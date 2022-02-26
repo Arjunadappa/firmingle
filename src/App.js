618acd30fb97a268d5aa025995ed54966090a152
Arjun <aadappa@akamai.com>
2022-02-26 10:55:42 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.jpeg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class="bg-white shadow-lg">
+			<div class="max-w-7xl mx-auto">
+				<div class="flex justify-between space-x-7">
+					<div>
+						<a href="#" class="flex items-center py-4 px-2">
+							<img src={logo} alt="Logo" class="h-8 w-36 mr-2" />
+						</a>
+					</div>
+
+					<div class="hidden md:flex basis-2/5 justify-between items-center ">
+						<a
+							href=""
+							class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
+						>
+							Home
+						</a>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+						>
+							Services
+						</a>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+						>
+							About
+						</a>
+						<button class="py-2 px-2 text-gray-500 hover:text-white font-semibold  hover:bg-green-700 rounded-full transition duration-300 ">
+							Contact Us
+						</button>
+					</div>
+				</div>
+
+				{/* <div class="hidden md:flex items-center space-x-3 ">
+						<a
+							href=""
+							class="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300"
+						>
+							Log In
+						</a>
+						<a
+							href=""
+							class="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300"
+						>
+							Sign Up
+						</a>
+					</div> */}
+
+				<div class="md:hidden flex items-center">
+					<button class="outline-none mobile-menu-button">
+						<svg
+							class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+				</div>
+			</div>
+
+			<div class="hidden mobile-menu">
+				<ul class="">
+					<li class="active">
+						<a
+							href="index.html"
+							class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+						>
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							href="#services"
+							class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+						>
+							Services
+						</a>
+					</li>
+					<li>
+						<a
+							href="#about"
+							class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+						>
+							About
+						</a>
+					</li>
+					<li>
+						<a
+							href="#contact"
+							class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+						>
+							Contact Us
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
