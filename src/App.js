b4d99f4184cd49a93d47fc17af09f8f9eb94d8b4
Arjun <aadappa@akamai.com>
2022-02-26 22:50:53 +0530

    clientsLogo slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header.jsx';
 import Features from './components/features';
+import Carousel from './components/clientCarousel';
 function App() {
 	return (
 		<div>
 			<Header></Header>
+			<Carousel></Carousel>
 			<Features></Features>
 		</div>
 	);
---
+++ src/components/clientCarousel.jsx
@@ -0,0 +1,68 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import googleLogo from '../google.png';
+import microsoftLogo from '../microsoft.png';
+import amazonLogo from '../amazon.png';
+import 'swiper/css';
+
+
+import { Autoplay} from 'swiper';
+
+export default function Carousel() {
+	return (
+		<>
+			<Swiper
+				slidesPerView={1}
+				spaceBetween={10}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				breakpoints={{
+					640: {
+						slidesPerView: 2,
+						spaceBetween: 20,
+					},
+					768: {
+						slidesPerView: 4,
+						spaceBetween: 40,
+					},
+					1024: {
+						slidesPerView: 5,
+						spaceBetween: 50,
+					},
+				}}
+				modules={[Autoplay]}
+				className="mySwiper mt-20 mx-auto max-w-6xl"
+			>
+				<SwiperSlide>
+					<img src={microsoftLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={googleLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={amazonLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={microsoftLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={microsoftLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={amazonLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={googleLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={microsoftLogo}></img>
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={microsoftLogo}></img>
+				</SwiperSlide>
+			</Swiper>
+		</>
+	);
+}
