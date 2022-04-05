52edbe1c3c57dc1496a1882b34efb34c887be8aa
Ashrafrs77 <ashrafrs777@gmail.com>
2022-04-05 10:58:20 +0530

    Updated

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,7 +1,7 @@
 
 function info(){
     return (
-        <div className=" flex h-60 bg-firmingleOrange mt-10 text-[#0A0A0A] text-5xl leading-tight">
+        <div className=" font-sans flex h-60 bg-firmingleOrange mt-10 text-[#0A0A0A] text-5xl leading-tight">
             <div className="basis-4/12 py-12 text-center">
                 750+ <br></br>Projects Completed
             </div>
